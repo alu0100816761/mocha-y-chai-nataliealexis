54f372e4a2d4d2fa527454efe363e813e3890a9c
alu0100818369 <Natalie@users-MacBook-Pro-251.local>
2016-03-16 09:27:19 +0000

    Añadidas pruebas para la clase Temperatura, superadas

--- conversor_test.js
+++ conversor_test.js
@@ -13,3 +13,17 @@ describe("Clase Medida", function () {
     });
   });
 });
+
+describe("Clase Temperatura", function() {
+  describe("constructor temperatura", function() {
+    it("deberia tener un constructor", function() {
+      var temperatura = new Temperatura (32, 'c');
+      expect(temperatura.valor).to.equal(32);
+    });
+    it("deberia poder recibir un solo parametro", function() {
+      var temperatura = new Temperatura ('32 c');
+      expect(temperatura.valor).to.equal(32);
+      expect(temperatura.tipo).to.equal('c');
+    });
+  });
+});
