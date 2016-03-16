a8cec950329a53ff38b7bf3b8132e089c9647c8e
alu0100816761 <alu0100816761@ull.edu.es>
2016-03-16 09:45:58 +0000

    Pruebas para la clase Fahrenheit. Superadas.

--- conversor_test.js
+++ conversor_test.js
@@ -14,7 +14,7 @@ describe("Clase Medida", function () {
   });
 });
 
-describe("Clase Temperatura", function() {
+describe("Clase Temperatura", function () {
   describe("constructor temperatura", function() {
     it("deberia tener un constructor", function() {
       var temperatura = new Temperatura (32, 'c');
@@ -24,6 +24,25 @@ describe("Clase Temperatura", function() {
       var temperatura = new Temperatura ('32 c');
       expect(temperatura.valor).to.equal(32);
       expect(temperatura.tipo).to.equal('c');
+    });
+  });
+});
+
+describe("Clase Fahrenheit", function () {
+  describe("constructor fahrenheit", function () {
+    it("deberia tener un constructor", function () {
+      var far = new Fahrenheit (320e-1);
+      expect(far.valor).to.equal(320e-1);
+    });
+  });
+  describe("funciones de fahrenheit", function () {
+    it("debe convertir a celsius", function () {
+      var far = new Fahrenheit (320e-1);
+      expect(far.toCelsius()).to.equal(0);
+    });
+    it("debe convertir a kelvin", function () {
+      var far = new Fahrenheit (320e-1);
+      expect(far.toKelvin()).to.equal(273.15);
     });
   });
 });
