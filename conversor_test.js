97df72475dd9838d738df7d874f47c579b49236b
alu0100816761 <alu0100816761@ull.edu.es>
2016-03-16 09:16:16 +0000

    Arreglada la prueba del constructor. Funciona

--- conversor_test.js
+++ conversor_test.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 describe("Conversor", function () {
   describe("constructor medida", function() {
     it("deberia tener un constructor", function() {
-      var medida = new Medida (32, f);
+      var medida = new Medida (32, 'f');
       expect(medida.valor).to.equal(32);
     });
   });
